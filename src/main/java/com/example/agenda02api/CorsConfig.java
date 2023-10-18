fa33d7d5d5bf2b541ff8bc2e8d75fd7fc8e14662
jonathasmatos <jonathasvirtual@gmail.com>
2023-10-18 14:01:40 -0300

    status

---
+++ src/main/java/com/example/agenda02api/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.example.agenda02api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081") // Permita o acesso do seu front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
